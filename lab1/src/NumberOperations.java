f372714a49e4e6e8bdebd2bb455dbe22bebec836
Ivanna Vasylchuk <ivasilc23@gmail.com>
2024-09-16 10:22:31 +0300

    lab_1

---
+++ lab1/src/NumberOperations.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class NumberOperations {
+    public static void main(String[] args) {
+        List<Number> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(20.5);
+        numbers.add(30.121);
+        numbers.add(40.7f);
+        numbers.add(50L);
+        numbers.add(60.3);
+        numbers.add(70);
+        numbers.add(80.1);
+        numbers.add(90);
+        numbers.add(100.9);
+        numbers.add(new BigDecimal("123.6"));
+
+        System.out.println("Оригінальні числа:");
+        for (Number num : numbers) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        System.out.println("\nЧисла у форматі цілих:");
+        for (Number num : numbers) {
+            System.out.print(num.intValue() + " ");
+        }
+        System.out.println();
+
+        System.out.println("\nОкруглені числа:");
+        for (Number num : numbers) {
+            BigDecimal rounded;
+            if (num instanceof BigDecimal) {
+                rounded = ((BigDecimal) num).setScale(0, RoundingMode.HALF_UP);
+            } else {
+                rounded = new BigDecimal(num.toString()).setScale(0, RoundingMode.HALF_UP);
+            }
+            System.out.print(rounded + " ");
+        }
+        System.out.println();
+
+        System.out.println("\nЧисла з 2 знаками після коми:");
+        for (Number num : numbers) {
+            System.out.printf("%.2f ", num.doubleValue());
+        }
+        System.out.println();
+
+        List<Integer> integers = new ArrayList<Integer>();
+        List<Double> doubles = new ArrayList<Double>();
+        List<Float> floats = new ArrayList<Float>();
+        List<Long> longs = new ArrayList<Long>();
+
+        for (Number num : numbers) {
+            if (num instanceof Integer) {
+                integers.add((Integer) num);
+            } else if (num instanceof Double) {
+                doubles.add((Double) num);
+            } else if (num instanceof Float) {
+                floats.add((Float) num);
+            } else if (num instanceof Long) {
+                longs.add((Long) num);
+            }
+        }
+
+        System.out.println("\nРозділення за типами:");
+        System.out.println("Integers: " + integers);
+        System.out.println("Doubles: " + doubles);
+        System.out.println("Floats: " + floats);
+        System.out.println("Longs: " + longs);
+
+        Number max = numbers.get(0);
+        for (Number num : numbers) {
+            if (num.doubleValue() > max.doubleValue()) {
+                max = num;
+            }
+        }
+        System.out.println("\nНайбільше число: " + max);
+
+        System.out.println("\nДемонстрація BigDecimal:");
+        BigDecimal bd1 = new BigDecimal("0.1");
+        BigDecimal bd2 = new BigDecimal("0.2");
+        BigDecimal sum = bd1.add(bd2);
+        System.out.println("0.1 + 0.2 = " + sum);
+        System.out.println("0.1 + 0.2 з округленням до 2 знаків = " +
+                sum.setScale(2, RoundingMode.HALF_UP));
+    }
+}
