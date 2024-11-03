8a3ff24f52ef7f0ff2b12d392425d607a79b7d5f
Ivanna Vasylchuk <ivasilc23@gmail.com>
2024-11-03 23:30:10 +0200

    lab3 done

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -1,16 +1,65 @@
 package com.gildedrose;
 
-public class Item {
-    public String name;
-    public ItemData itemData;
+class Item {
+    private final ItemName name;
+    private final ItemData itemData;
 
     public Item(String name, int sellIn, int quality) {
-        this.name = name;
+        this.name = new ItemName(name);
         this.itemData = new ItemData(new SellIn(sellIn), new Quality(quality));
+    }
+
+    public String getNameString() {
+        return name.getValue();
+    }
+
+    public void decreaseSellIn() {
+        itemData.decreaseSellIn();
+    }
+
+    public void increaseQuality() {
+        itemData.increaseQuality();
+    }
+
+    public void decreaseQuality() {
+        itemData.decreaseQuality();
+    }
+
+    public void resetQuality() {
+        itemData.resetQuality();
+    }
+
+    public boolean isExpired() {
+        return itemData.isSellInExpired();
+    }
+
+    public boolean sellInLessThan(int days) {
+        return itemData.sellInLessThan(days);
     }
 
     @Override
     public String toString() {
-        return this.name + ", " + this.itemData;
+        return name + ", " + itemData;
+    }
+
+    public String getName() {
+        return name.getValue();
+    }
+}
+
+class ItemName {
+    private final String value;
+
+    public ItemName(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
     }
 }
