3dc53b66cf0feea03c0905a9be165cd0edbd5a3c
Ivanna Vasylchuk <ivasilc23@gmail.com>
2024-11-03 12:37:19 +0200

    A SellIn class with a value field is created.

---
+++ Java/src/main/java/com/gildedrose/SellIn.java
@@ -0,0 +1,13 @@
+package com.gildedrose;
+
+public class SellIn {
+    private int value;
+
+    public SellIn(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
