22d431d859a7f83950edd9bd54005890c993ec34
Ivanna Vasylchuk <ivasilc23@gmail.com>
2024-11-03 23:11:14 +0200

    Encapsulation of fields: All fields are now private and the ItemData class provides methods to manage SellIn and Quality. lab3 done

--- Java/src/main/java/com/gildedrose/ItemData.java
+++ Java/src/main/java/com/gildedrose/ItemData.java
@@ -1,12 +1,36 @@
 package com.gildedrose;
 
-public class ItemData {
-    public SellIn sellIn;
-    public Quality quality;
+class ItemData {
+    private final SellIn sellIn;
+    private final Quality quality;
 
     public ItemData(SellIn sellIn, Quality quality) {
         this.sellIn = sellIn;
         this.quality = quality;
+    }
+
+    public void decreaseSellIn() {
+        sellIn.decrease();
+    }
+
+    public void increaseQuality() {
+        quality.increase();
+    }
+
+    public void decreaseQuality() {
+        quality.decrease();
+    }
+
+    public void resetQuality() {
+        quality.reset();
+    }
+
+    public boolean isSellInExpired() {
+        return sellIn.isExpired();
+    }
+
+    public boolean sellInLessThan(int days) {
+        return sellIn.isLessThan(days);
     }
 
     @Override
