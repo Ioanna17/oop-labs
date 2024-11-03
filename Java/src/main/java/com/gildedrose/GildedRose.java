c9bce50783a514902b96e4e7e3ca5623ff202a05
Ivanna Vasylchuk <ivasilc23@gmail.com>
2024-11-03 16:55:47 +0200

    Let's go back to the previous version. i don't like it

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,27 +1,74 @@
 package com.gildedrose;
 
 class GildedRose {
-    private final ItemCollection itemCollection;
+    Item[] items;
 
     public GildedRose(Item[] items) {
-        this.itemCollection = new ItemCollection(items);
+        this.items = items;
     }
 
     public void updateQuality() {
-        itemCollection.updateAllItems();
+        for (Item item : items) {
+            if (isLegendary(item)) {
+                continue;
+            }
+            if (isAgedBrie(item)) {
+                updateAgedBrie(item);
+                continue;
+            }
+            if (isBackstagePass(item)) {
+                updateBackstagePass(item);
+                continue;
+            }
+            updateRegularItem(item);
+        }
     }
 
-    private static class ItemCollection {
-        private final Item[] items;
+    private boolean isLegendary(Item item) {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
 
-        public ItemCollection(Item[] items) {
-            this.items = items;
+    private boolean isAgedBrie(Item item) {
+        return item.name.equals("Aged Brie");
+    }
+
+    private boolean isBackstagePass(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    private void updateAgedBrie(Item item) {
+        if (item.quality.getValue() < 50) {
+            item.quality.increase();
         }
+        item.sellIn.decrease();
+        if (item.sellIn.isExpired() && item.quality.getValue() < 50) {
+            item.quality.increase();
+        }
+    }
 
-        public void updateAllItems() {
-            for (Item item : items) {
-//                item.updateQuality();
+    private void updateBackstagePass(Item item) {
+        if (item.quality.getValue() < 50) {
+            item.quality.increase();
+            if (item.sellIn.isLessThan(11) && item.quality.getValue() < 50) {
+                item.quality.increase();
             }
+            if (item.sellIn.isLessThan(6) && item.quality.getValue() < 50) {
+                item.quality.increase();
+            }
+        }
+        item.sellIn.decrease();
+        if (item.sellIn.isExpired()) {
+            item.quality.reset();
+        }
+    }
+
+    private void updateRegularItem(Item item) {
+        if (item.quality.getValue() > 0) {
+            item.quality.decrease();
+        }
+        item.sellIn.decrease();
+        if (item.sellIn.isExpired() && item.quality.getValue() > 0) {
+            item.quality.decrease();
         }
     }
 }
