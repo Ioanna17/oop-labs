e62fbc47985a48ee1df712e04f439c91d75cf934
Ivanna Vasylchuk <ivasilc23@gmail.com>
2024-11-03 12:20:25 +0200

    Created the Quality class with the value field and limiting the range of quality values.

---
+++ Java/src/main/java/com/gildedrose/Quality.java
@@ -0,0 +1,14 @@
+package com.gildedrose;
+
+public class Quality {
+    private int value;
+    private static final int MAX_QUALITY = 50;
+
+    public Quality(int value) {
+        this.value = Math.min(value, MAX_QUALITY);
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
