f0e9513d1c1e93cb5e397b19e129b10c6c8ec83f
Ivanna Vasylchuk <ivasilc23@gmail.com>
2024-11-03 20:47:56 +0200

    make fixes for the tests to work correctly 2

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -1,12 +1,12 @@
 package com.gildedrose;
 
 public class Item {
-    public Name name;
+    public String name;
     public SellIn sellIn;
     public Quality quality;
 
     public Item(String name, int sellIn, int quality) {
-        this.name = new Name(name);
+        this.name = name;
         this.sellIn = new SellIn(sellIn);
         this.quality = new Quality(quality);
     }
