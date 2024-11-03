280d9fe0e9d7993163636da34ba146c7f46c51a6
Ivanna Vasylchuk <ivasilc23@gmail.com>
2024-11-03 16:44:45 +0200

    Moved the First-Class Collection ItemCollection as an internal class in GildedRose.Delegated processing of the items array through the updateAllItems() method in the ItemCollection

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,74 +1,27 @@
 package com.gildedrose;
 
 class GildedRose {
-    Item[] items;
+    private final ItemCollection itemCollection;
 
     public GildedRose(Item[] items) {
-        this.items = items;
+        this.itemCollection = new ItemCollection(items);
     }
 
     public void updateQuality() {
-        for (Item item : items) {
-            if (isLegendary(item)) {
-                continue;
+        itemCollection.updateAllItems();
+    }
+
+    private static class ItemCollection {
+        private final Item[] items;
+
+        public ItemCollection(Item[] items) {
+            this.items = items;
+        }
+
+        public void updateAllItems() {
+            for (Item item : items) {
+//                item.updateQuality();
             }
-            if (isAgedBrie(item)) {
-                updateAgedBrie(item);
-                continue;
-            }
-            if (isBackstagePass(item)) {
-                updateBackstagePass(item);
-                continue;
-            }
-            updateRegularItem(item);
-        }
-    }
-
-    private boolean isLegendary(Item item) {
-        return item.name.equals("Sulfuras, Hand of Ragnaros");
-    }
-
-    private boolean isAgedBrie(Item item) {
-        return item.name.equals("Aged Brie");
-    }
-
-    private boolean isBackstagePass(Item item) {
-        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
-
-    private void updateAgedBrie(Item item) {
-        if (item.quality.getValue() < 50) {
-            item.quality.increase();
-        }
-        item.sellIn.decrease();
-        if (item.sellIn.isExpired() && item.quality.getValue() < 50) {
-            item.quality.increase();
-        }
-    }
-
-    private void updateBackstagePass(Item item) {
-        if (item.quality.getValue() < 50) {
-            item.quality.increase();
-            if (item.sellIn.isLessThan(11) && item.quality.getValue() < 50) {
-                item.quality.increase();
-            }
-            if (item.sellIn.isLessThan(6) && item.quality.getValue() < 50) {
-                item.quality.increase();
-            }
-        }
-        item.sellIn.decrease();
-        if (item.sellIn.isExpired()) {
-            item.quality.reset();
-        }
-    }
-
-    private void updateRegularItem(Item item) {
-        if (item.quality.getValue() > 0) {
-            item.quality.decrease();
-        }
-        item.sellIn.decrease();
-        if (item.sellIn.isExpired() && item.quality.getValue() > 0) {
-            item.quality.decrease();
         }
     }
 }
