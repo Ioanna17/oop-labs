d0e13b0247b7a01a679a6192ea78c3fb9ff27d1d
Ivanna Vasylchuk <ivasilc23@gmail.com>
2024-11-10 18:09:44 +0200

    add public class InventoryItem

--- Java02/src/main/java/com/gildedrose/GildedRose.java
+++ Java02/src/main/java/com/gildedrose/GildedRose.java
@@ -9,6 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
+            InventoryItem InventoryItem = new InventoryItem(item);
             updateItem(item);
         }
     }
