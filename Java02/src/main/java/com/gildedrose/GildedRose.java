346039012c03d705c8252c7e1f8669746bf5f865
Ivanna Vasylchuk <ivasilc23@gmail.com>
2024-11-10 11:23:18 +0200

    add private static void updateQualityForAgedBrieAndTickets

--- Java02/src/main/java/com/gildedrose/GildedRose.java
+++ Java02/src/main/java/com/gildedrose/GildedRose.java
@@ -28,24 +28,8 @@ class GildedRose {
     private void updateQualityItem(Item item) {
         if (item.name.equals("Aged Brie")
             || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            } else {
+            updateQualityForAgedBrieAndTickets(item);
+        } else {
             if (item.quality > 0) {
                 if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                     item.quality = item.quality - 1;
@@ -53,6 +37,27 @@ class GildedRose {
             }
         }
     }
+
+    private static void updateQualityForAgedBrieAndTickets(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+        }
+    }
+
     private void updeteExperation(Item item){
 
         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
