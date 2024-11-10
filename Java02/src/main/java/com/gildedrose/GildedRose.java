f1c61f2ae22fe1909145374f612ed602d63dae6f
Ivanna Vasylchuk <ivasilc23@gmail.com>
2024-11-10 17:39:17 +0200

    update updateQualityItem

--- Java02/src/main/java/com/gildedrose/GildedRose.java
+++ Java02/src/main/java/com/gildedrose/GildedRose.java
@@ -30,16 +30,13 @@ class GildedRose {
             increaseQuality(item);
         }
         else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+            increaseQuality(item);
+            if (item.sellIn < 11) {
+                increaseQuality(item);
+            }
 
-                if (item.sellIn < 11) {
-                    increaseQuality(item);
-                }
-
-                if (item.sellIn < 6) {
-                    increaseQuality(item);
-                }
+            if (item.sellIn < 6) {
+                increaseQuality(item);
             }
         } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 return;
