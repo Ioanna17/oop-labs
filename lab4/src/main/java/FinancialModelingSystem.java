a474dc31b4082e4eaea164f984038173c1321584
Ivanna Vasylchuk <ivasilc23@gmail.com>
2024-12-16 09:34:33 +0200

    апдейт

--- lab4/src/main/java/FinancialModelingSystem.java
+++ lab4/src/main/java/FinancialModelingSystem.java
@@ -2,13 +2,11 @@ public class FinancialModelingSystem {
     public static void main(String[] args) {
         FinancialInstrumentRegistry registry = new FinancialInstrumentRegistry();
 
-        // Реєстрація прототипів
         Stock defaultStock = new Stock("акція", 100.0);
         registry.addPrototype("stock", defaultStock);
         registry.addPrototype("bond", new Bond("облігація", 1000.0, 0.05));
         registry.addPrototype("derivative", new Derivative("дериватив", defaultStock, 95.0));
 
-        // Створення та симуляція інструментів
         FinancialInstrument stock = registry.createPrototype("stock");
         stock.simulate();
         stock.calculateTax();
