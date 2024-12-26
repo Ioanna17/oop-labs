df58e87b051f5c2db85257265b2707a9c19da075
Ivanna Vasylchuk <ivasilc23@gmail.com>
2024-12-26 15:18:11 +0200

    update MediaIterator

--- lab5/src/main/java/MediaIterator.java
+++ lab5/src/main/java/MediaIterator.java
@@ -1,29 +1,31 @@
-import java.util.*;
+import java.util.Iterator;
+import java.util.List;
+
 
 class MediaIterator implements Iterator<ImageProxy> {
-    private Iterator<ImageProxy> iterator;
-    private final List<ImageProxy> mediaList;
+    private Iterator<ImageProxy> imageIterator;
+    private final List<ImageProxy> imageList;
 
     public MediaIterator(List<ImageProxy> mediaList) {
-        this.mediaList = mediaList;
-        this.iterator = mediaList.iterator();
+        this.imageList = mediaList;
+        this.imageIterator = mediaList.iterator();
     }
 
     public void setFilterKeyword(String keyword) {
-        this.iterator = this.mediaList.stream()
-                .filter(i -> i.getClass().getSimpleName().toLowerCase().contains(keyword))
-                .iterator();
+        this.imageIterator = this.imageList.stream().filter(i-> i.name.toLowerCase().contains(keyword)).iterator();
     }
 
     public void resetIterator() {
-        this.iterator = this.mediaList.iterator();
+        this.imageIterator = this.imageList.iterator();
     }
 
+    @Override
     public boolean hasNext() {
-        return iterator.hasNext();
+        return this.imageIterator.hasNext();
     }
 
+    @Override
     public ImageProxy next() {
-        return iterator.next();
+            return imageIterator.next();
     }
 }
