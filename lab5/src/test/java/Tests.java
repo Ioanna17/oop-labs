b0620c5f483b51ed234e08667e6b378e0b4e2b46
Ivanna Vasylchuk <ivasilc23@gmail.com>
2024-12-26 15:09:55 +0200

    Test for MediaIterator

--- lab5/src/test/java/Tests.java
+++ lab5/src/test/java/Tests.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.*;
 import static org.mockito.Mockito.*;
+import javax.swing.*;
+import java.awt.*;
 
 class Tests {
     private ImageComponent imageComponent;
@@ -16,11 +18,31 @@ class Tests {
         Icon newIcon = mock(Icon.class);
         imageComponent.setIcon(newIcon);
 
-        // Create a mock Graphics object to test paintComponent
         Graphics g = mock(Graphics.class);
         imageComponent.paintComponent(g);
 
-        // Verify that the new icon's paintIcon method was called
+        verify(newIcon).paintIcon(eq(imageComponent), eq(g), anyInt(), anyInt());
+    }
+
+    private ImageComponent imageComponent;
+    private Icon mockIcon;
+
+    @BeforeEach
+    void setUp() {
+        mockIcon = mock(Icon.class);
+        imageComponent = new ImageComponent(mockIcon);
+    }
+
+    @Test
+    void testSetIcon() {
+        Icon newIcon = mock(Icon.class);
+        imageComponent.setIcon(newIcon);
+
+        Graphics g = mock(Graphics.class);
+        imageComponent.paintComponent(g);
+
         verify(newIcon).paintIcon(eq(imageComponent), eq(g), anyInt(), anyInt());
     }
 }
+
+
